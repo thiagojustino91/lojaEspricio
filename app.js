613d3f7f450d8e14380ccc65e673f389ab6a8227
unknown <thiago.lima.justino@gmail.com>
2025-11-08 17:16:25 -0300

    atualizacao08.11

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 const express =require("express");
+const cookieParser = require("cookie-parser");
 require('dotenv').config();
 const app = express();
 const {produtoRoutes}= require("./src/routes/produtoRoutes");
 const {clienteRoutes} = require("./src/routes/clienteRoutes")
 
-const PORT =process.env.PORT
+const PORT = process.env.PORT
 
 app.use(express.json());
+app.use(cookieParser());
+
 
 // Rotas da aplicação
 app.use('/', produtoRoutes);
